05b39ab9449891f4d5fa7edd64a928f4322aaa18
devansh-kumar-2214 <95587604+devansh-kumar-2214@users.noreply.github.com>
2023-12-01 07:01:38 +0530

    Update assis.js

--- assis.js
+++ assis.js
@@ -27,12 +27,15 @@ function wishMe() {
 window.addEventListener("load", () => {
   let p = document.createElement("p");
    p = document.createElement("p");
-      p.classList.add("replay");
-      p.innerText = "hey! i am motherbee";
-      texts.appendChild(p);
+  
 
   speak("hey! i am motherbee");
   speak("and i am here to assist on maternal care");
+   let p = document.createElement("p");
+   p = document.createElement("p");
+      p.classList.add("replay");
+      p.innerText = "Hey! i am motherbee and i am here to assist on maternal care";
+      texts.appendChild(p);
   wishMe();
 });
 
@@ -95,13 +98,32 @@ speak(p.innerText);
       texts.appendChild(p);
     speak(p.innerText);
     }
+
+    if (text.includes("travel")&&text.includes("during") && text.includes("pregnancy")) {
+      p = document.createElement("p");
+      p.classList.add("replay");
+      p.innerText =
+        "In many cases, it's safe to travel during pregnancy, especially in the early and middle stages. However, it's crucial to discuss travel plans with your healthcare provider, especially if you're at risk for complications";
+      texts.appendChild(p);
+    speak(p.innerText);
+    }
+
+    if (text.includes("prenatal tests")&&text.includes("tests") && text.includes("pregnancy")) {
+      p = document.createElement("p");
+      p.classList.add("replay");
+      p.innerText =
+        "Routine prenatal tests include blood tests, ultrasounds, and screenings for conditions like gestational diabetes. Your healthcare provider will recommend specific tests based on your health and medical history";
+      texts.appendChild(p);
+    speak(p.innerText);
+    }
     
   }
 });
 
 recognition.addEventListener("end", () => {
+  wait(2000)
   recognition.start();
    p = document.createElement("p");
 });
-
+setTimeout(recognition.start(),3000);
 recognition.start();
